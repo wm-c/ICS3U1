f37a9181cda4155010c931aa91bff9e5712ea7db
William Meathrel <wcmeathrel@gmail.com>
2020-03-04 12:47:32 -0500

    Small Change

--- defcon/src/main/java/defcon/defcon.java
+++ defcon/src/main/java/defcon/defcon.java
@@ -1,14 +1,27 @@
 package defcon;
 
+import javax.swing.JFrame;
+
+
 public class defcon{
 
+    public JFrame game;
+
     public static void main(String[] args) {
-       new defcon;
+       new defcon();
     }
 
     public defcon(){
-        
+        System.out.println("DEFCON 5");
     }
+
+    private void init(){
+        game = new JFrame();
+        g
+    }
+
+
+    
 
 
 
